69e65fce1fed9c92b5b2c6b5290d59af6230f74a
Ski11 <eliaslarsson058@gmail.com>
2023-11-27 19:25:04 +0100

    Små fixar

--- BlueLine/src/pages/index.js
+++ BlueLine/src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
+      description="Den text baserade verisonen utav lspdfr installationen <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
