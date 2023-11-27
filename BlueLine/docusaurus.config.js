67e6c498a6a806b995e74aa6d80695d09d2634e3
Ski11 <eliaslarsson058@gmail.com>
2023-11-27 19:05:38 +0100

    fixat

--- BlueLine/docusaurus.config.js
+++ BlueLine/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/icon.ico',
 
   // Set the production url of your site here
-  url: 'https://Ski11-Pro.github.io',
+  url: 'https://ski11-pro.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/blueline/',
